2abd556e3c64cd660472bce8a476b79b6ee774f9
guanghuixu <1643206826@qq.com>
2020-02-22 09:27:57 +0800

    draw performance

---
+++ draw_error.py
@@ -0,0 +1,25 @@
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+log_path = './logs'
+A_acc = 84.14
+means = [A_acc]
+for idx in range(1, 10, 1):
+    log_file = os.path.join(log_path, 'B{}_log.txt'.format(idx))
+    with open(log_file) as f:
+        lines = f.readlines()
+    acc_list = []
+    for line in lines:
+        if 'new_acc:' in line:
+            # print(line)
+            acc = re.search('new_acc: .{1,20}%', line).group()
+            acc = acc.replace('new_acc:', '').replace('%', '').replace(' ', '')
+            acc_list.append(float(acc))
+    mean = np.mean(acc_list)
+    std_deviation = np.std(acc_list)
+    means.append(mean)
+    plt.errorbar(idx, mean, yerr=std_deviation, fmt="o")
+plt.plot(np.arange(0, 10, 1), means)
+plt.savefig('./output.png', bbox_inches = 'tight')
